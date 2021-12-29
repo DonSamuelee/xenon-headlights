072893bea8272cc71e30211057a8c4c1aabe2718
DonSamuele <66538187+DonSamuelee@users.noreply.github.com>
2021-12-29 19:19:03 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,11 +1,4 @@
-ESX = nil
-
-Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
-end)
+ESX = exports['es_extended']:getSharedObject()
 
 RegisterNetEvent('fari:bianchi')
 AddEventHandler('fari:bianchi', function()
